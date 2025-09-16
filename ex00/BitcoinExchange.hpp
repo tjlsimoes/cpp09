2795132010c9d22e6723e426a9daa5b6892498ef
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2025-09-16 19:58:15 +0100

    Sketch ex00 solution

---
+++ ex00/BitcoinExchange.hpp
@@ -0,0 +1,19 @@
+#ifndef BITCOIN_EXCHANGE_HPP
+# define BITCOIN_EXCHANGE_HPP
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <sstream>
+#include <string>
+
+class BitcoinExchange
+{
+	public:
+		BitcoinExchange();
+		~BitcoinExchange();
+
+
+};
+
+#endif
