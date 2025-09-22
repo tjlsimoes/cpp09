c102ee10ff590ec6784f9205de383fed3a43f974
tjorge-l <tjorge-l@student.42lisboa.com>
2025-09-22 13:38:53 +0100

    Sketch correct solution for ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -19,93 +19,74 @@ bool	valid_input(std::string expr)
 	return (true);
 }
 
-std::stack<char>	gen_stack(std::string user_input)
+std::stack<int>	calculate(std::string user_input)
 {
-	std::stack<char>	stack;
+	std::stack<int>	stack;
+	int					left;
+	int					right;
 
 	for (std::string::iterator it = user_input.begin(); it != user_input.end(); it++)
 	{
-		std::cout << *it << std::endl;
+		// std::cout << *it << std::endl;
 		if (!std::isspace(*it))
-			stack.push(*it);
-	}
-	return (stack);
-}
-
-void	get_operands(char *left, char *right, char *oper, std::stack<char> &stack)
-{
-	*oper = stack.top();
-	stack.pop();
-	*right = stack.top();
-	stack.pop();
-	*left = stack.top();
-	if (std::isdigit(*left))
-		stack.pop();
-}
-
-int	calculate(std::stack<char> &stack)
-{
-	char	oper;
-	char	left;
-	char	right;
-
-	get_operands(&left, &right, &oper, stack);
-	std::cout
-	<< "Left: " << left << "\n"
-	<< "Right: " << right << "\n"
-	<< "Oper: " << oper
-	<< std::endl;
-	if (!std::isdigit(left))
-	{
-		std::cout << "here" << std::endl;
-		switch (oper)
 		{
-			case '+':
-				return (calculate(stack) + (right - '0'));
-			case '-':
-				return (calculate(stack) - (right - '0'));
-			case '*':
-				return (calculate(stack) * (right - '0'));
-			case '/':
-				return (calculate(stack) / (right - '0'));
-			default:
-				throw std::exception();
+			if (std::isdigit(*it))
+				stack.push(*it - '0');
+			else
+			{
+				// Check for errors here?
+				right = stack.top();
+				stack.pop();
+				left = stack.top();
+				stack.pop();
+				// std::cout
+				// << "Left: " << left << "\n"
+				// << "Right: " << right
+				// << std::endl;
+				switch (*it)
+				{
+					case '+':
+						stack.push(left + right);
+						break ;
+					case '-':
+						stack.push(left - right);
+						break ;
+					case '*':
+						stack.push(left * right);
+						break ;
+					case '/':
+						if (right == 0)
+							throw std::exception();
+						stack.push(left / right);
+						break ;
+					default:
+						throw std::exception();
+				}
+			}
 		}
 	}
-	switch (oper)
-	{
-		case '+':
-			return ((left - '0') + (right - '0'));
-		case '-':
-			return ((left - '0') - (right - '0'));
-		case '*':
-			return ((left - '0') * (right - '0'));
-		case '/':
-			return ((left - '0') / (right - '0'));
-		default:
-			throw std::exception();
-	}
+	return stack;
 }
 
 int	main(int argc, char **argv)
 {
-	std::stack<char> stack;
 	if (argc != 2 || !valid_input(argv[1]))
 		return (1);
-	stack = gen_stack(argv[1]);
-	std::cout << std::endl;
-	std::cout << "Size: " << stack.size() << std::endl;
-	std::cout << std::endl;
-	// size_t i = 0;
-	// size_t size = stack.size();
-	// while (i < size)
-	// {
-	// 	std::cout << stack.top() << std::endl;
-	// 	stack.pop();
-	// 	i++;
-	// }
-	int result = calculate(stack);
-	std::cout << "Result: " << result << std::endl;
+	valid_input(argv[1]);
+	std::stack<int> stack;
+	try
+	{
+		std::stack<int> stack;
+		stack = calculate(argv[1]);
+
+		if (stack.size() != 1)
+			std::cerr << "Error" << std::endl;
+		std::cout << "Result: " << stack.top() << std::endl;
+	}
+	catch(...)
+	{
+		std::cerr << "Error" << '\n';
+	}
 
 	return (0);
 }
